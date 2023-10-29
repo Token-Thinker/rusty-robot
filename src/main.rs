38fe87e6f167d8cdbcf2a0bbbe24cc04095250ad
Michael Guerrier <tokenthinkers@gmail.com>
2023-10-29 14:03:52 -0400

    +ledc breathe

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,65 @@ use embassy_time::{Duration, Timer};
 // use esp_wifi::{initialize, EspWifiInitFor};
 pub mod prelude;
 
+use hal::clock::Clocks;
 #[allow(unused_imports)]
 use prelude::*;
 
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+static CLOCKS: StaticCell<Clocks> = StaticCell::new();
+static LEDC: StaticCell<LEDC> = StaticCell::new();
+static HSTIMER0: StaticCell<hal::ledc::timer::Timer<'static, HighSpeed>> = StaticCell::new();
+
+/*pub fn global_executor() -> Executor {
+    if EXECUTOR.uninit(){
+        todo!()
+    }
+
+    EXECUTOR.init(Executor::new());
+}
+*/
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let mut system = peripherals.DPORT.split();
-    let clocks = ClockControl::max(system.clock_control).freeze();
+
+    let clocks_control = ClockControl::max(system.clock_control).freeze();
+    let clocks = CLOCKS.init(clocks_control);
+
+    let ledc = LEDC.init_with(|| {
+        LEDC::new(peripherals.LEDC, clocks, &mut system.peripheral_clock_control)
+    });
 
     let io = gpio::IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let led_pin = io.pins.gpio4.into_push_pull_output();
     let led = io.pins.gpio12.into_push_pull_output();
 
-    let ledc = LEDC::new(peripherals.LEDC, &clocks, &mut system.peripheral_clock_control);
-    
+    let hstimer0 = HSTIMER0.init_with( || {
+        ledc.get_timer::<HighSpeed>(timer::Number::Timer0)
+    });
+
+    hstimer0
+    .configure(timer::config::Config {
+        duty: timer::config::Duty::Duty5Bit,
+        clock_source: timer::HSClockSource::APBClk,
+        frequency: 24u32.kHz(),
+    })
+    .unwrap();
+
+    let mut channel0 = ledc.get_channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: hstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
+
+    channel0.start_duty_fade(0, 100, 2000).expect_err(
+        "Fading from 0% to 100%, at 24kHz and 5-bit resolution, over 2 seconds, should fail",
+    );
+
     let timer_group0 = TimerGroup::new(
         peripherals.TIMG0,
         &clocks,
@@ -39,7 +81,7 @@ fn main() -> ! {
 
     executor.run(|spawner| {
         spawner.spawn(blink(led_pin)).ok();
-        spawner.spawn(breathe(led,ledc)).ok();
+        spawner.spawn(breathe(channel0)).ok();
     })
 }
 
@@ -57,33 +99,8 @@ async fn blink(mut led_pin: GpioPin<Output<PushPull>, 4>) {
 }
 
 #[embassy_macros::task]
-async fn breathe(led: GpioPin<Output<PushPull>, 12>, ledc: LEDC<'_>) {    
-    let mut hstimer0 = ledc.get_timer::<HighSpeed>(timer::Number::Timer0);
-
-    hstimer0
-    .configure(timer::config::Config {
-        duty: timer::config::Duty::Duty5Bit,
-        clock_source: timer::HSClockSource::APBClk,
-        frequency: 24u32.kHz(),
-    })
-    .unwrap();
-
-    let mut channel0 = ledc.get_channel(channel::Number::Channel0, led);
-    channel0
-        .configure(channel::config::Config {
-            timer: &hstimer0,
-            duty_pct: 10,
-            pin_config: channel::config::PinConfig::PushPull,
-        })
-        .unwrap();
-
-    channel0.start_duty_fade(0, 100, 2000).expect_err(
-        "Fading from 0% to 100%, at 24kHz and 5-bit resolution, over 2 seconds, should fail",
-    );
-    
+async fn breathe(channel0: hal::ledc::channel::Channel<'static, HighSpeed, GpioPin<Output<PushPull>, 12>> ) {        
     loop {
-        // Set up a breathing LED: fade from off to on over a second, then
-        // from on back off over the next second.  Then loop.
         channel0.start_duty_fade(0, 100, 1000).unwrap();
         while channel0.is_duty_fade_running() {}
         
