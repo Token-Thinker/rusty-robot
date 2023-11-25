0f76340262f727a1270b47a5259607eb0a50ceb9
Michael Guerrier <token.thinkers@gmail.com>
2023-11-25 14:24:45 -0500

    servo_calculations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+/* 
+
+Hardware - GPIO structure
+GPIO2 - Flywheel Motors (High/Low)
+GPI012 - Launcher Servo
+GPIO13 - Tilt Servo
+GPIO16 - Pan Servo
+ 
+Aux - GPIOs
+GPIO4 - LED indicator Feedback
+GPIO 2/4/12/13/14/15 - microSD Recording subprocess [requires other gpios to be disabled] 
+
+*/
+
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use embassy_time::Timer;
 use esp_println::println;
 use hal::{get_core, clock::Clocks};
 
@@ -21,7 +36,7 @@ use prelude::*;
 
 #[embassy_executor::task]
 async fn control_led(
-    mut led: GpioPin<Output<PushPull>, 12>,
+    mut led: GpioPin<Output<PushPull>, 4>,
     control: &'static Signal<CriticalSectionRawMutex, bool>,
 ) {
     println!("Starting control_led() on core {}", get_core() as usize);
@@ -38,41 +53,69 @@ async fn control_led(
 
 #[embassy_executor::task]
 async fn control_servo(
-    servo: GpioPin<Output<PushPull>, 4>,
+    servo: GpioPin<Output<PushPull>, 12>,
     ledc: &'static LEDC<'_>,
 ){
-    let mut hstimer0 = ledc.get_timer::<HighSpeed>(timer::Number::Timer0);
+    println!("Initializing control_servo task");
 
-    hstimer0
+    let mut hstimer1 = ledc.get_timer::<HighSpeed>(timer::Number::Timer1);
+    println!("Timer configured");
+
+    hstimer1
     .configure(timer::config::Config {
-        duty: timer::config::Duty::Duty5Bit,
+        duty: timer::config::Duty::Duty14Bit,
         clock_source: timer::HSClockSource::APBClk,
-        frequency: 24u32.kHz(),
+        frequency: 50u32.Hz(),
     })
     .unwrap();
+    println!("Timer initialization complete");
 
-    let mut channel0 = ledc.get_channel(channel::Number::Channel0, servo);
-    channel0
+    // Function to convert angle to duty cycle
+    fn angle_to_duty(angle: u8) -> u32 {
+        let min_pulse_ms = 0.5; // 1ms pulse width for 0 degrees
+        let max_pulse_ms = 2.4; // 2ms pulse width for 180 degrees
+    
+        let min_duty = (min_pulse_ms / 20.0) * 16383.0;
+        let max_duty = (max_pulse_ms / 20.0) * 16383.0;
+    
+        let duty_range = max_duty - min_duty;
+        let duty_14bit = min_duty + (duty_range * (angle as f32 / 180.0));
+    
+        duty_14bit as u32
+    }
+
+    let mut channel1 = ledc.get_channel(channel::Number::Channel1, servo);
+    println!("Channel configured");
+
+    channel1
         .configure(channel::config::Config {
-            timer: &hstimer0,
+            timer: &hstimer1,
             duty_pct: 10,
             pin_config: channel::config::PinConfig::PushPull,
         })
         .unwrap();
+    println!("Channel initialization complete, starting sweep");
 
-    channel0.start_duty_fade(0, 100, 2000).expect_err(
-        "Fading from 0% to 100%, at 24kHz and 5-bit resolution, over 2 seconds, should fail",
-    );
-
+    // Sweeping loop
     loop {
-        channel0.start_duty_fade(0, 100, 1000).unwrap();
-        while channel0.is_duty_fade_running() {}
-        
-        channel0.start_duty_fade(100, 0, 1000).unwrap();
-        while channel0.is_duty_fade_running() {}
-    }
+        // Sweep from 0 to 180 degrees
+        for angle in 0..=180u8 {
+            let duty = angle_to_duty(angle);
+            channel1.set_duty_hw(duty);
+            println!("Sweeping up - Angle: {}, Duty: {}", angle, duty);
+            Timer::after(Duration::from_millis(10)).await;
+        }
 
+        // Sweep back from 180 to 0 degrees
+        for angle in (0..=180u8).rev() {
+            let duty = angle_to_duty(angle);
+            channel1.set_duty_hw(duty);
+            println!("Sweeping down - Angle: {}, Duty: {}", angle, duty);
+            Timer::after(Duration::from_millis(10)).await;
+        }
+    }
 }
+
 
 
 #[main]
@@ -93,8 +136,8 @@ async fn main(_spawner: Spawner) -> ! {
     let mut cpu_control = CpuControl::new(system.cpu_control);
     let led_ctrl_signal = &*make_static!(Signal::new());
 
-    let led = io.pins.gpio12.into_push_pull_output();
-    let servo = io.pins.gpio4.into_push_pull_output();
+    let led = io.pins.gpio4.into_push_pull_output();
+    let servo = io.pins.gpio12.into_push_pull_output();
     let ledc = make_static!(LEDC::new(peripherals.LEDC, clocks));
 
     let cpu1_fnctn = move || {
