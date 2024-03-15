7505d094757c4b934e5b95e6d05a3c575961a714
Michael Guerrier <tokenthinkers@gmail.com>
2024-03-15 00:18:15 -0400

    clean up

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -1,1 +1,9 @@
-pub mod hw;
+//! ## Hardware Module
+//!
+//! This module provides the interface for interacting with the application's hardware components. It includes submodules for specific hardware control:
+//!
+//! * **motor_control:** Functions for controlling a motor (On, Off, Launch).
+//! * **servo_control:**  Fine-grained servo control, including configuration, angle mapping, and smooth movement. 
+
+pub mod servo_ctrl;
+pub mod motor_ctrl;
